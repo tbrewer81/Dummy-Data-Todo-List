022d68a60a808532b5ac201b2c380bcc48631302
tbrewer81 <brewertasha2@gmail.com>
2021-12-03 20:08:26 -0600

    unfiltered

---
+++ dummy.js
@@ -0,0 +1,46 @@
+   // We'll pre-populate this array with a couple objects just so it's not undefined if your internet connection isn't working properly.
+
+let arrayOfTodos = [
+    {
+    "userId": 14,
+    "id": 1,
+    "title": "delectus aut autem",
+    "completed": false
+},
+{
+    "userId": 20,
+    "id": 2,
+    "title": "delectus aut autem",
+    "completed": false
+}]
+
+const fetchTodos = () => {
+    fetch('https://jsonplaceholder.typicode.com/todos')
+    .then( (response) => response.json())
+    .then( (json) => arrayOfTodos = json)
+}
+
+const logTodos = () => {
+    console.log(arrayOfTodos)
+}
+
+const populateTodos = () => {
+    let todoList = document.getElementById('todo-list');
+    let todoTitle = ""
+    for (i=0; 1 < arrayOfTodos.length; i++) {
+        todoTitle = document.createTextNode(arrayOfTodos[i].title);
+        let todoItem = document.createElement("li")
+        if (arrayOfTodos[i].completed) {
+        todoItem.classList.add("istrue");
+        } else {
+        todoItem.classList.add("isfalse");
+    }
+    todoItem.appendChild(todoTitle);
+    todoList.appendChild(todoItem);
+    }
+}
+
+console.log(arrayOfTodos[0].userId) // => 14
+console.log(arrayOfTodos[1].userId) // => 20
+    // end of for loop
+    // end of function
